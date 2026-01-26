5f66011bde4f033f42e4a25da711e484315a990f
SuminaAnastasya95 <suminan1995@gmail.com>
2026-01-26 19:21:34 +0300

    Описание функций

--- 6-crm/orders.py
+++ 6-crm/orders.py
@@ -1,14 +1,18 @@
 def create_order():
+    """Создание заказа"""
     pass
 
 
 def list_orders():
+    """Перечень заказов"""
     pass
 
 
 def edit_order():
+    """Изменения заказа"""
     pass
 
 
 def remove_order():
+    """Удаление заказа"""
     pass
